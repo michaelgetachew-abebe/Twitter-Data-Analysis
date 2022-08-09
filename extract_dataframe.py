a1c285b92cee6cbf39df229a1b225c62fed8c8de
michaelgetachew <michaelgetachew@zhewutech.com>
2022-08-09 17:58:22 +0300

    feat( functionality ) added code to the function find_statuses_count function

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -36,7 +36,9 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count 
+         statuses_count = [tw['user']['statuses_count']
+                          for tw in self.tweets_list]
+        return statuses_count
         
     def find_full_text(self)->list:
         text = 
